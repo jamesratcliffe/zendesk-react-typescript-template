5fbf528c67798ec67afd0dc3189e6b4d04e75175
James Ratcliffe <james.ratcliffe@lightspeedhq.com>
2021-09-13 15:18:52 -0400

    Update App to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from '@zendeskgarden/react-theming';
 import { Grid } from '@zendeskgarden/react-grid';
